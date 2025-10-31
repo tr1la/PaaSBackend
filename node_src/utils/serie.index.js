ea29c5db4e0ec0a38d681625fd1625ad689570a3
tr1la <phucuongto2004@gmail.com>
2025-10-31 13:16:32 +0700

    delete nodejs src

--- node_src/utils/serie.index.js
+++
@@ -1,9 +0,0 @@
-// createIndex.js
-const { connectToDatabase } = require("./mongodb");
-
-(async () => {
-  const db = await connectToDatabase();
-  const serieCollection = db.collection("series");
-
-  await serieCollection.createIndex({ serie_title: "text" });
-})();
